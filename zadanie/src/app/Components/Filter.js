fcd360a5e382249dc164eaeb54fa44f3577047c6
wiktorlemanski <wiktorlem1@wp.pl>
2024-11-22 16:48:12 +0100

    filter dzila

--- zadanie/src/app/Components/Filter.js
+++ zadanie/src/app/Components/Filter.js
@@ -1,14 +1,16 @@
 'use client';
 import React from 'react';
+import {useAmountsContext} from "@/app/providers/AmountProvider";
 
-export default function Filter({ categories, selectedCategory, onCategoryChange }) {
+export default function Filter({categories}) {
+    const {  selectedCategory, setSelectedCategory } = useAmountsContext()
     return (
         <div>
             <label style={labelStyle} htmlFor="category-filter">Filtruj według kategorii:</label>
             <select
                 id="category-filter"
                 value={selectedCategory}
-                onChange={e => onCategoryChange(e.target.value)}
+                onChange={e => setSelectedCategory(e.target.value)}
             >
                 <option value="">Wszystkie</option>
                 {categories.map((category, index) => (
